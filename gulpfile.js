3d5b34eebf808d5eb65dac592d721260eb25c6d1
Dietschweiler, Patrik <pdietsch@hsr.ch>
2016-05-15 18:45:03 +0200

    gulpfile tyscript extension

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var del         = require('del');
+var ts 			= require('gulp-typescript');
+var merge 		= require('merge2');
 
 // Remove build directory.
 gulp.task('clean', function(cb) {
@@ -25,6 +27,21 @@ gulp.task('sass', function() {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest("./dist/app/css"))
         .pipe(browserSync.stream());
+});
+
+gulp.task('ts', function(){
+	gulp.task('scripts', function() {
+	var tsResult = gulp.src('lib/**/*.ts')
+		.pipe(ts({
+			declaration: true,
+			noExternalResolve: true
+		}));
+ 
+	return merge([
+		tsResult.dts.pipe(gulp.dest('./dist/app/ts/definitions')),
+		tsResult.js.pipe(gulp.dest('.dist/app/js'))
+	]);
+});
 });
 
 // Copy all resources that are not Sass files into build directory.
