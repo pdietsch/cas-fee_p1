7f660251eab5e8ee6e96f10e65cc7ef09206f0d1
Dietschweiler, Patrik <pdietsch@hsr.ch>
2016-05-16 22:42:02 +0200

    priority icon change

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -12,8 +12,38 @@ $( document ).ready(function() {
         }
     );
 });
+
+$( document ).ready(function() {
+  $("span[class^='priority-']").hover(
+  function(){
+    var prioritySelected = getNumberOfNextCharacters($(this).attr("class"), $(this).attr("class").indexOf("priority-") + 9)
+    for(var i = 1; i <= prioritySelected; i++){
+      var statusElement = $('.priority-'+i);
+      statusElement.addClass('ion-ios-bolt');
+      statusElement.removeClass('ion-ios-bolt-outline');
+    }
+  },
+  function(){
+    var prioritySelected = getNumberOfNextCharacters($(this).attr("class"), $(this).attr("class").indexOf("priority-") + 9)
+    for(var i = 1; i <= prioritySelected; i++){
+      var statusElement = $('.priority-'+i);
+      statusElement.addClass('ion-ios-bolt-outline');
+      statusElement.removeClass('ion-ios-bolt');
+    }
+  }
+  );
+});
+
+function getNumberOfNextCharacters(str, index){
+  var start = index;
+  while(!isNaN(str[index])){
+    index ++;
+  }
+  return str.substring(start ,index );
+}
+
 /* Fallback input type date not supported */
-$(function() {
+$( document ).ready(function() {
   if (!Modernizr.inputtypes.date) {
     $('input[type=date]').datepicker({
       // Consistent format with the HTML5 picker
