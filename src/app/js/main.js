55158e6e3ed49d0de5ee5f3726a7cff8411e4931
Dietschweiler, Patrik <pdietsch@hsr.ch>
2016-05-16 21:28:52 +0200

    style add.html fix select bug introduce jquery datepicker as fallback

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -12,3 +12,13 @@ $( document ).ready(function() {
         }
     );
 });
+/* Fallback input type date not supported */
+$(function() {
+  if (!Modernizr.inputtypes.date) {
+    $('input[type=date]').datepicker({
+      // Consistent format with the HTML5 picker
+      dateFormat: 'yy-mm-dd'
+    });
+  }
+});
+
