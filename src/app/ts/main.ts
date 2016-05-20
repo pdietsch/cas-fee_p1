3c02a81cdfbbd427341b76bff75908d911a19b78
Dietschweiler, Patrik <pdietsch@hsr.ch>
2016-05-20 17:55:09 +0200

    template handlebars

--- src/app/ts/main.ts
+++ src/app/ts/main.ts
@@ -3,15 +3,10 @@
  */
 /// <reference path="./todo.ts"/>
 /// <reference path="./services/mock-todos.ts"/>
+    
 var initHtml : string;
 initHtml = "";
-
-var template = Handlebars.compile('<div role="listitem" class="listitem"><div class="date"><div class="duedate">' +
-    '{{dueDateAsString}}</div><div class="status {{#if finished}}finished{{else}}pending{{/if}}"><span class="status-icon' +
-    ' {{#if finished}}ion-ios-checkmark{{else}}ion-ios-circle-outline{{/if}}"></span><span    class="status-text">Finished</span></div>' +
-    '</div><div class="todo"><div class="title">{{title}}' +
-    '{{#each priority}}<span class="ion-ios-bolt">{{/each}}</span><span class="ion-ios-bolt"></span></div>' +
-    '<div class="desc">{{description}}</div></div><div class="actions">    <button>Edit</button>    </div>    </div>');
+var template = this["P1"]["templates"]["note"]
 todoList.todos.forEach((currentNote) => {
 
     initHtml += template(currentNote);
