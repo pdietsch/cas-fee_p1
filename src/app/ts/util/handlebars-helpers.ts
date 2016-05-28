21cb031be67ad5c48cd3175997b05be74b70df0e
Matthias Gemperli <hello@matthiasgemperli.ch>
2016-05-28 16:42:34 +0200

    Changed css and handlebars-helpers
    - Changed css for button in todolist
    - Removed log in handlebars-helpers

--- src/app/ts/util/handlebars-helpers.ts
+++ src/app/ts/util/handlebars-helpers.ts
@@ -7,7 +7,6 @@ Handlebars.registerHelper('times', function(n :number, block:any ) {
 
 Handlebars.registerHelper('ifCond', function(v1 : any) {
     var options : any = arguments[arguments.length - 1];
-    console.log(arguments[0], arguments[1]);
     if(arguments[0] <= arguments[1]) {
         return options.fn(this);
     }
